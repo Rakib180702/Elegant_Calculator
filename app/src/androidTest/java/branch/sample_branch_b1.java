6a918ea76d26cf96826c2c6ab5c5d193ca346e5b
RAKIB <rakib1807025@stud.kuet.ac.bd>
2022-05-11 00:03:18 +0600

    modify b1 from master

--- app/src/androidTest/java/branch/sample_branch_b1.java
+++ app/src/androidTest/java/branch/sample_branch_b1.java
@@ -2,7 +2,49 @@ package branch;
 
 public class sample_branch_b1
 {
+    String a;
+
+
+
     String name, id;
+
     float cgpa;
-    
+    public sample_branch_b1(String a, String name, String id, float cgpa) {
+        this.a = a;
+        this.name = name;
+        this.id = id;
+        this.cgpa = cgpa;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public float getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
+    }
 }
